05807774ce32353c4fa81ee2f23ecd9f0bb25b57
Rakesh Prasad <rakeshrocco28@gmail.com>
2025-10-09 21:19:52 +0530

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import cors from 'cors';
 import rateLimit from "express-rate-limit";
 import mongoSanitize from "express-mongo-sanitize";
 import { connectDB } from "./src/config/db.js";
@@ -10,6 +11,7 @@ import carRoutes from "./src/routes/car.routes.js";
 import bidRoutes from "./src/routes/bid.routes.js";
 
 const app = express();
+app.use(cors());
 app.use(helmet());
 const port = process.env.PORT || 5000;
 const limiter = rateLimit({
